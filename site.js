b1bb9b806baf6a092a7034d949cb196bca8c1fb9
bemabadi swathi <130271827+bembadiswathi-5c6@users.noreply.github.com>
2023-06-05 22:14:17 +0530

    fist comment

--- site.js
+++ site.js
@@ -1,1 +1,19 @@
-alert("Swathi's page");
+var data1={image:"./Images/ABC.png",name: "Swathi", designation: "Student"};
+var data2={image:"./Images/DEF.jpeg",name: "bembadi swathi reddy", designation: "Intern"};
+var isdata1=true;
+var display_data;
+var change_my_pic=function(){
+    if(isdata1)
+    {
+        display_data=data2;
+        isdata1=false;
+    }
+    else
+    {
+        display_data=data1;
+        isdata1=true;
+    }
+    document.getElementById("my_image").src=display_data.image;
+    document.getElementById("my_name").innerHTML=display_data.name;
+    document.getElementById("my_designation").innerHTML=display_data.designation;
+}
